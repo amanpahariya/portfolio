83d1d789f67abc1ce23e100ea73e1d7a78e89886
amanpahariya <amangupta2786@gmail.com>
2021-02-11 21:50:03 +0530

    Responsive About Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
@@ -10,7 +11,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={Home} />
+        <Route path="/about" exact component={About} />
         <Route path="/resume" exact component={Home} />
         <Route path="/contact-us" exact component={Home} />
       </Switch>
---
+++ src/components/About/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import image from '../../about.jpg';
+import './index.css'
+
+function About() {
+    return (
+        <>
+            <div className="topContainer">
+                <div className="about">
+                    <img src={image} />
+                </div>
+                <div className="about-box">
+                    <h1 className="about-tittle">About Me</h1>
+                    <ul className="about-info">
+                        <li><span>Name:</span> <span>Shainki Gupta</span></li>
+                        <li><span>Date of birth:</span> <span>August 15, 1997</span></li>
+                        <li><span>Address:</span> <span>Near KhatuShyam Mandir, C-21 Mall, MR-9, Indore(M.P).</span></li>
+                        <li><span>Email:</span> <span>shanki000gupta@gmail.com</span></li>
+                        <li><span>Phone: </span> <span>+91-7828072833,9340874488 </span></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="bottomContainer">
+                <h1>More About Me</h1>
+                <p>
+                    Seeking position as Senior Business
+                    Development Executive at BestPeers
+                    Infosystem Pvt. Ltd to utilize 3.5 years of
+                    experience, focused attention to detail, and
+                    demonstrated ability to maximizecompany
+                    profits through trategic
+                    marketing.
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default About;
